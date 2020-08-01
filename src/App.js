6ef00bb8e07e0d8f0e2f9d30f64fc81ce68c1c20
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:09:42 -0400

    Retirado url da função getresults no useEffect

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import Footer from './components/Footer'
 import data from './data/dados_iniciais.json'
 
 function App() {
-  const [content, setContent] = useState(data);
+  const [content] = useState(data);
   const [categories, setCategories] = useState([])
-  const [mainVideo, setMainVideo] = useState({})
-
-  //const [mainVideo, setMainVideo] = useState({})
-
-
 
 
   useEffect(() => {
     const getresults = () =>{
-      let result = []
-      content.categorias.map(category => {
-        result.push(category)
+      
+      let result = content.categorias.map(category => {
+        const {titulo, cor, link, link_extra, videos} = category;
+        return {
+          titulo,
+          cor,
+          link,
+          link_extra,
+          videos
+        }
+        
       })
       setCategories(result)
     }
@@ -34,10 +37,8 @@ function App() {
       videoDescription={"Written by Shiro Sagisu, Hideaki Anno & Mike Wyzgowski"}
       videoTitle={ "Platina Jazz - Come, Sweet Death [Komm, süsser Tod] (from ”Neon Genesis Evangelion”)"}/>
      {categories.map((category, id) => {
-       const {titulo, cor, link, link_extra, videos} = category;
-       return <Carousel key={id} category={category} />
+       return <Carousel key={id} category={category} /> 
      })}
-      {console.log(mainVideo)}
      
       <Footer />
     </div>
