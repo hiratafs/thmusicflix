71ede21463c15ca4f0593199545d1596ef7e57a9
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:10:31 -0400

    Footer colocado abaixo de Main

--- src/PageDefault/index.js
+++ src/PageDefault/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import Menu from '../components/Menu';
 import Footer from '../components/Footer';
+import styled from 'styled-components'
+
+const Main = styled.main`
+    background-color: var(--black);
+    color: var(--white);
+    flex: 1;
+    padding-top: 5%;
+    padding-left: 5%;
+    padding-right: 5%
+`;
 
 
 export default function PageDefault({children}) {
     return (
-        <div>
+        <>
+        <Main>
             <Menu />
             {children}
-            <Footer />
-        </div>
+        </Main>
+        <Footer />
+        </>
     )
 }
