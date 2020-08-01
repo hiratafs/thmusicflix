8f4759d9de2fa160d8cb46495e05ae89cfa361e9
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:15:08 -0400

    Colocado Link do react router dom

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import Logo from '../../assets/img/logo.png'
+import Logo from '../../assets/img/logo.png';
+import {Link} from 'react-router-dom';
 import './Menu.css'
 import ButtonLink from '../Menu/components/ButtonLink'
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-               <img src={Logo} className="Logo" alt="Logo da MusicFlix"></img>
-            </a>
-            <ButtonLink className="ButtonLink" href="/">
+            <Link to="/">
+                <img src={Logo} className="Logo" alt="Logo da MusicFlix"></img>
+            </Link>
+ 
+            <ButtonLink className="ButtonLink">
+                <Link to='/cadastro/video' >
                 Novo Vídeo
+                </Link>
             </ButtonLink>
         </nav>
     )
