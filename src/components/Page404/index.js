8fd2b63862426cec24bb4da40001347524b1cced
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:12:17 -0400

    Criada page 404

---
+++ src/components/Page404/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Página não encontrada!</h1>
+            <p>Você errou o endereço!</p>
+        </div>
+    )
+}
