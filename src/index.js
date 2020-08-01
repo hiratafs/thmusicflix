dea74f394adeb7b930f58d15a5eeb1d22a84c6bd
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:16:47 -0400

    Instalação do react router dom e criação das routes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from '../src/pages/cadastro/Video';
+import CadastroCategoria from '../src/pages/cadastro/CadastroCategoria'
+import Page404 from './components/Page404';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Page404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
