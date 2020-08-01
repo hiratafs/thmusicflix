86d1d0b05ed6c9b52f781c168f1575c2283697d6
Tatiane Hirata <tatihirata@outlook.com>
2020-07-31 21:17:24 -0400

    Instalado Link do react router dom

--- src/pages/cadastro/Video/index.js
+++ src/pages/cadastro/Video/index.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import PageDefault from '../../../PageDefault'
+import { Link } from 'react-router-dom'
+
+
 
 export default function CadastroVideo() {
     return (
-        <div>
+        <>
+        <PageDefault>
+            <h1>Cadastro de vídeo</h1>
+            <Link to='/cadastro/categoria'>
+           Cadastrar categoria
+            </Link>
+        </PageDefault>
+        </>
             
-        </div>
+        
     )
 }
